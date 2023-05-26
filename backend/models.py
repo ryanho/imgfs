60f926b1f3164a0089fc127d8acd3a43a0a8baae
Ryan Ho <204075+ryanho@users.noreply.github.com>
2023-05-26 12:12:59 +0800

    新增圖片紀錄用model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from customauth.models import User
 
 # Create your models here.
+
+
+class AbstractImageModel(models.Model):
+    cid = models.CharField(verbose_name='ipfs cid', max_length=46, primary_key=True)
+    filename = models.CharField(verbose_name='filename', max_length=1024)
+    width = models.PositiveIntegerField(verbose_name='image width', null=True)
+    height = models.PositiveIntegerField(verbose_name='image height', null=True)
+    size = models.PositiveIntegerField(verbose_name='file size', null=True)
+    content_type = models.CharField(verbose_name='content type', max_length=1024)
+    created = models.DateTimeField(verbose_name='created date', auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class UploadImage(AbstractImageModel):
+    user = models.ForeignKey(User, verbose_name='user', null=True, blank=True, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Upload image'
+        verbose_name_plural = 'Upload images'
+
+
+class ThumbnailImage(AbstractImageModel):
+    origin = models.OneToOneField(UploadImage, verbose_name='origin image', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Thumbnail image'
+        verbose_name_plural = 'Thumbnail images'
