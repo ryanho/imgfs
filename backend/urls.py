f90ab49c707565b83896d3f6fcfb0d84aab67018
Ryan Ho <204075+ryanho@users.noreply.github.com>
2023-05-30 10:47:33 +0800

    把全站cache改成per view

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import HomeView, ShowImageView, get_image
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='HomeView'),
-    path('<str:cid>', ShowImageView.as_view(), name='ShowImageView'),
-    path('<str:cid>/<str:filename>', get_image, name='GetImage'),
+    path('', cache_page(60 * 60)(HomeView.as_view()), name='HomeView'),
+    path('<str:cid>', cache_page(60 * 60)(ShowImageView.as_view()), name='ShowImageView'),
+    path('<str:cid>/<str:filename>', cache_page(60 * 60)(get_image), name='GetImage'),
 ]
