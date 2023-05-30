225f163aa33f303384349c211678ca607a6c8e02
Ryan Ho <204075+ryanho@users.noreply.github.com>
2023-05-30 16:17:23 +0800

    拿掉首頁的快取設定

--- backend/urls.py
+++ backend/urls.py
@@ -3,7 +3,7 @@ from .views import HomeView, ShowImageView, get_image
 from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    path('', cache_page(60 * 60)(HomeView.as_view()), name='HomeView'),
+    path('', HomeView.as_view(), name='HomeView'),
     path('<str:cid>', cache_page(60 * 60)(ShowImageView.as_view()), name='ShowImageView'),
     path('<str:cid>/<str:filename>', cache_page(60 * 60)(get_image), name='GetImage'),
 ]
