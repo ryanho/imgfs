6d244d98be97317b4857d66b518773466371a953
Ryan Ho <204075+ryanho@users.noreply.github.com>
2023-06-02 17:03:59 +0800

    新增刪除圖片功能（WIP）

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ImageListView, UserListView
+from .views import ImageListView, UserListView, DeleteImageView
 
 urlpatterns = [
     path('', ImageListView.as_view(), name='ImageListView'),
+    path('<str:cid>/delete/', DeleteImageView.as_view(), name='DeleteImageView'),
     path('user/', UserListView.as_view(), name='UserListView')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DeleteView
 from backend.models import UploadImage, ThumbnailImage
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from customauth.models import User
+import httpx
 
 # Create your views here.
 
@@ -25,6 +26,32 @@ class ImageListView(LoginRequiredMixin, ListView):
         if request.htmx:
             self.template_name = 'dashboard/components/_partial_image_list.html'
         return super().get(request, *args, **kwargs)
+
+
+class DeleteImageView(LoginRequiredMixin, DeleteView):
+    model = UploadImage
+
+    def form_valid(self, form):
+        api = settings.IPFS_API + '/api/v0'
+        with httpx.Client as client:
+            # delete thumbnail
+            url = f'{api}/api/v0/pin/rm?arg={self.object.thumbnailimage.cid}'
+            res = client.post(url)
+            if res.status_code == 200:
+                self.object.thumbnailimage.delete()
+
+            # delete image
+            url = f'{api}/api/v0/pin/rm?arg={self.object.cid}'
+            res = client.post(url)
+            if res.status_code == 200:
+                self.object.delete()
+
+            # ipfs gc
+            url = f'{api}/api/v0/repo/gc'
+            res = client.post(url)
+            if res.status_code == 200:
+                print(res.text)
+            return redirect('ImageListView')
 
 
 class UserListView(ListView):
